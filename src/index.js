fe5c7a71a0812fcd4fa831da8e547814dc9a0d4c
rafamsantos28 <rafaelmsantos28@icloud.com>
2025-12-07 01:22:09 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+// url=https://github.com/rafamsantos28/antplay-plus/blob/main/src/index.js
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./../public/main.css";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<React.StrictMode><App /></React.StrictMode>);
