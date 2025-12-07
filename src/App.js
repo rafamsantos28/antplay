66da9a5facc61ddd77a21d946078593fbb9b5cf8
rafamsantos28 <rafaelmsantos28@icloud.com>
2025-12-07 01:11:49 +0000

    Set up React Router with multiple routes

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
+// url=https://github.com/rafamsantos28/antplay-plus/blob/main/src/App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import MovieDetail from "./pages/MovieDetail";
+import Player from "./pages/Player";
+import Login from "./pages/Login";
+import ProfileManagement from "./pages/ProfileManagement";
 
+const App = () => (
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movies" element={<Movies />} />
+      <Route path="/movie/:id" element={<MovieDetail />} />
+      <Route path="/player/:id" element={<Player />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/profiles" element={<ProfileManagement />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
